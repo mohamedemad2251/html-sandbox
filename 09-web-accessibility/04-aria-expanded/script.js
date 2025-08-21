b6eeed66a2eaba110e28989d0284256201af7210
mohamedemad2251 <mohamedemad22521@gmail.com>
2025-08-21 16:05:29 +0300

    Section 13: Web Accessibility Introduction (DONE)

--- 09-web-accessibility/04-aria-expanded/script.js
+++ 09-web-accessibility/04-aria-expanded/script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
+//  Target the accordion header when the DOM is loaded.
   const header = document.querySelector('.accordion-header');
-
+//  Listen to a click, if clicked, do the following logic:
   header.addEventListener('click', () => {
+    // Get the next sibling to the accordion header (which is the accordion-content at this stage)
     const content = header.nextElementSibling;
+    // Check if the expanded option is true (if expanded or collapsed)
     const expanded = header.getAttribute('aria-expanded') === 'true';
 
     // Toggle aria-expanded attribute
